b4e4b21d7abd4aebfbac66de6d495671fbb2d39a
Ian Armstrong <50608249+armstrongi@users.noreply.github.com>
2025-02-18 22:06:14 +0000

    System changes

--- AccelerateDevGitHubCopilot/src/Library.Infrastructure/obj/Debug/net8.0/Library.Infrastructure.AssemblyInfo.cs
+++ AccelerateDevGitHubCopilot/src/Library.Infrastructure/obj/Debug/net8.0/Library.Infrastructure.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Library.Infrastructure")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+530af81cf96ec8172887cfdaaf5745157e8367f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbd9a47c2cf820fd5bb64eb4f7c1b99e3773f821")]
 [assembly: System.Reflection.AssemblyProductAttribute("Library.Infrastructure")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Library.Infrastructure")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- AccelerateDevGitHubCopilot/tests/UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
+++ AccelerateDevGitHubCopilot/tests/UnitTests/obj/Debug/net8.0/UnitTests.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+530af81cf96ec8172887cfdaaf5745157e8367f6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bbd9a47c2cf820fd5bb64eb4f7c1b99e3773f821")]
 [assembly: System.Reflection.AssemblyProductAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UnitTests")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
